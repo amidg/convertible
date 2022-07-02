c1a3da94f7b1f68b716b90200d4620b47bff88f6
amidg <amidg4x4dima@gmail.com>
2022-07-02 14:24:48 +0000

    folder management work in progress

--- run_ffmpeg.sh
+++ run_ffmpeg.sh
@@ -2,12 +2,33 @@
 
 #ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i Season\ 01/\[Beatrice-Raws\]\ Darling\ in\ the\ Franxx\ 01\ \[BDRip\ 1920x1080\ x264\ FLAC\].mkv -i Season\ 01/RUS\ Sound/\[Beatrice-Raws\]\ Darling\ in\ the\ Franxx\ 01\ \[BDRip\ 1920x1080\ x264\ FLAC\].RUS.\[AniLibria\].mka -i Season\ 01/\[Beatrice-Raws\]\ Darling\ in\ the\ Franxx\ 01\ \[BDRip\ 1920x1080\ x264\ FLAC\].ENG.\[CR\].ass -map 0 -map 1 -map 2 -metadata:s:a:1 language=rus -metadata:s:s:2 language=eng -c:v hevc_nvenc Darling_In_the_Franxx_S01E01.mkv
 
+function select_folder {
+    echo "Let's find your content folder:"
+    array=(./*)
+    for dir in "${array[@]}"; do 
+        echo "$dir"; 
+    done
+    echo "There are ${#array[@]} folders in the current path"; \
+    select dir in "${array[@]}"; do 
+        #inner loop of select
+
+        echo "you selected ${dir}"; 
+        cd ${dir}
+        
+        if [ -f ${file_with_extension}.mkv ] || [ -f ${file_with_extension}.avi ] || [ -f ${file_with_extension}.mp4 ] ; then
+            DONE_PATH_TO_FOLDER=true
+            break;
+        fi
+    done
+}
+
 # path variables
 PATH_TO_MEDIA=/PlexMedia
-PATH_TO_SPECIFIC_CONTENT=/PlexMedia/Anime
+PATH_TO_SPECIFIC_CONTENT=$PATH_TO_MEDIA
 
 # variables
 NVIDIA_AVAILABLE=false
+DONE_PATH_TO_FOLDER=false
 
 echo "Welcome to automated FFMPEG script!"
 if [[ "$(inxi -G)" == *"NVIDIA"* ]]; then # check if Nvidia GPU is present
@@ -17,6 +38,16 @@ if [[ "$(inxi -G)" == *"NVIDIA"* ]]; then # check if Nvidia GPU is present
     echo $INXI_INFO_GPU | grep -o -P '(?<=Device-1:).*(?=Device-2)'
 fi
 
+cd $PATH_TO_MEDIA
+for file_with_extension in $PATH_TO_SPECIFIC_CONTENT; do 
+    if [ -f ${file_with_extension}.mkv ] || [ -f ${file_with_extension}.avi ] || [ -f ${file_with_extension}.mp4 ] ; then
+        DONE_PATH_TO_FOLDER=true
+        break;
+    fi
 
+    echo ; \
 
+    select_folder
+    
+done
 
