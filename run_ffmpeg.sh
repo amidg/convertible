e178c01bf9a8bca3a1da453da91d2856728fefb3
amidg <amidg4x4dima@gmail.com>
2022-07-02 14:40:15 +0000

    work in progress

--- run_ffmpeg.sh
+++ run_ffmpeg.sh
@@ -3,22 +3,33 @@
 #ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i Season\ 01/\[Beatrice-Raws\]\ Darling\ in\ the\ Franxx\ 01\ \[BDRip\ 1920x1080\ x264\ FLAC\].mkv -i Season\ 01/RUS\ Sound/\[Beatrice-Raws\]\ Darling\ in\ the\ Franxx\ 01\ \[BDRip\ 1920x1080\ x264\ FLAC\].RUS.\[AniLibria\].mka -i Season\ 01/\[Beatrice-Raws\]\ Darling\ in\ the\ Franxx\ 01\ \[BDRip\ 1920x1080\ x264\ FLAC\].ENG.\[CR\].ass -map 0 -map 1 -map 2 -metadata:s:a:1 language=rus -metadata:s:s:2 language=eng -c:v hevc_nvenc Darling_In_the_Franxx_S01E01.mkv
 
 function select_folder {
+    ITER=1
     echo "Let's find your content folder:"
     array=(./*)
     for dir in "${array[@]}"; do 
         echo "$dir"; 
     done
-    echo "There are ${#array[@]} folders in the current path"; \
     select dir in "${array[@]}"; do 
         #inner loop of select
+        if [ $ITER != 1 ] ; then
+            echo "Let's find your content folder:"
+            array=(./*)
+            for dir in "${array[@]}"; do 
+                echo "$dir"; 
+            done
+        fi
+
+        echo "There are ${#array[@]} folders in the current path"; \
 
         echo "you selected ${dir}"; 
         cd ${dir}
+
+        ITER=$ITER++
         
-        if [ -f ${file_with_extension}.mkv ] || [ -f ${file_with_extension}.avi ] || [ -f ${file_with_extension}.mp4 ] ; then
-            DONE_PATH_TO_FOLDER=true
-            break;
-        fi
+        #if [ -f *.mkv ] || [ -f ${file_with_extension}.avi ] || [ -f ${file_with_extension}.mp4 ] ; then
+        #    DONE_PATH_TO_FOLDER=true
+        #    break;
+        #fi
     done
 }
 
