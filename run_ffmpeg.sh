c0d73be6cf7e7c7c57a4e13234f1b80041daa452
amidg <amidg4x4dima@gmail.com>
2022-07-02 08:30:18 -0400

    wip

--- run_ffmpeg.sh
+++ run_ffmpeg.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+#ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i Season\ 01/\[Beatrice-Raws\]\ Darling\ in\ the\ Franxx\ 01\ \[BDRip\ 1920x1080\ x264\ FLAC\].mkv -i Season\ 01/RUS\ Sound/\[Beatrice-Raws\]\ Darling\ in\ the\ Franxx\ 01\ \[BDRip\ 1920x1080\ x264\ FLAC\].RUS.\[AniLibria\].mka -i Season\ 01/\[Beatrice-Raws\]\ Darling\ in\ the\ Franxx\ 01\ \[BDRip\ 1920x1080\ x264\ FLAC\].ENG.\[CR\].ass -map 0 -map 1 -map 2 -metadata:s:a:1 language=rus -metadata:s:s:2 language=eng -c:v hevc_nvenc Darling_In_the_Franxx_S01E01.mkv
+
+defaultPathToMedia=/PlexMedia
+updatedPathToFolder=/PlexMedia/Anime
+
 echo "Hello!"
