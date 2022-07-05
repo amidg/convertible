a3f7ac5313200e7318168200caaf89ab84a8f765
amidg <amidg4x4dima@gmail.com>
2022-07-05 02:16:35 +0000

    working recursive folder walking

--- run_ffmpeg.sh
+++ run_ffmpeg.sh
@@ -2,44 +2,29 @@
 
 #ffmpeg -hwaccel cuda -hwaccel_output_format cuda -i Season\ 01/\[Beatrice-Raws\]\ Darling\ in\ the\ Franxx\ 01\ \[BDRip\ 1920x1080\ x264\ FLAC\].mkv -i Season\ 01/RUS\ Sound/\[Beatrice-Raws\]\ Darling\ in\ the\ Franxx\ 01\ \[BDRip\ 1920x1080\ x264\ FLAC\].RUS.\[AniLibria\].mka -i Season\ 01/\[Beatrice-Raws\]\ Darling\ in\ the\ Franxx\ 01\ \[BDRip\ 1920x1080\ x264\ FLAC\].ENG.\[CR\].ass -map 0 -map 1 -map 2 -metadata:s:a:1 language=rus -metadata:s:s:2 language=eng -c:v hevc_nvenc Darling_In_the_Franxx_S01E01.mkv
 
-function select_folder {
-    ITER=1
-    echo "Let's find your content folder:"
-    array=(./*)
-    for dir in "${array[@]}"; do 
-        echo "$dir"; 
-    done
-    select dir in "${array[@]}"; do 
-        #inner loop of select
-        if [ $ITER != 1 ] ; then
-            echo "Let's find your content folder:"
-            array=(./*)
-            for dir in "${array[@]}"; do 
-                echo "$dir"; 
-            done
-        fi
-
-        echo "There are ${#array[@]} folders in the current path"; \
-
-        echo "you selected ${dir}"; 
-        cd ${dir}
-
-        ITER=$ITER++
-        
-        #if [ -f *.mkv ] || [ -f ${file_with_extension}.avi ] || [ -f ${file_with_extension}.mp4 ] ; then
-        #    DONE_PATH_TO_FOLDER=true
-        #    break;
-        #fi
-    done
-}
-
 # path variables
 PATH_TO_MEDIA=/PlexMedia
 PATH_TO_SPECIFIC_CONTENT=$PATH_TO_MEDIA
+PATH_TO_SUBS=$PATH_TO_SPECIFIC_CONTENT
+PATH_TO_AUDIO=$PATH_TO_SPECIFIC_CONTENT
 
 # variables
 NVIDIA_AVAILABLE=false
 DONE_PATH_TO_FOLDER=false
+
+# constants
+CUDA_REQUEST=' -hwaccel cuda -hwaccel_output_format cuda ' 
+
+function select_folder {
+    array=(./*)
+    echo "There are ${#array[@]} folders in the current path";
+    select dir in "${array[@]}"; do 
+        echo "you selected ${dir}"; 
+        echo "${dir}"
+        cd "${dir}"
+        break;
+    done
+}
 
 echo "Welcome to automated FFMPEG script!"
 if [[ "$(inxi -G)" == *"NVIDIA"* ]]; then # check if Nvidia GPU is present
@@ -50,15 +35,13 @@ if [[ "$(inxi -G)" == *"NVIDIA"* ]]; then # check if Nvidia GPU is present
 fi
 
 cd $PATH_TO_MEDIA
-for file_with_extension in $PATH_TO_SPECIFIC_CONTENT; do 
+for (( ; ; )) {
+    # infinite loop
+    select_folder # select folder until  you reach the destination
+
     if [ -f ${file_with_extension}.mkv ] || [ -f ${file_with_extension}.avi ] || [ -f ${file_with_extension}.mp4 ] ; then
         DONE_PATH_TO_FOLDER=true
         break;
     fi
-
-    echo ; \
-
-    select_folder
-    
-done
+}
 
