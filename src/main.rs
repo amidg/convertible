1645ca84555f34c7057a61cc65a9adfff6966e15
amidg <amidg4x4dima@gmail.com>
2022-10-01 08:43:27 -0700

    switched to Rust

---
+++ src/main.rs
@@ -0,0 +1,56 @@
+use gtk::prelude::*;
+use gtk::{Application, Button, ApplicationWindow};
+
+const APP_ID: &str = "org.gtk_rs.HelloWorld2";
+
+fn main() {
+    // Create a new application
+    let app = Application::builder().application_id(APP_ID).build();
+
+    // Connect to "activate" signal of `app`
+    app.connect_activate(build_ui);
+
+    // Run the application
+    app.run();
+}
+
+fn build_ui(app: &Application) {
+    // declare variable
+    let buttonLabel = "";
+
+    if buttonLabel == "" {
+        buttonLabel = "Press me!"
+    } else {
+        //
+    }
+
+    // Create a button with label and margins
+    let button = Button::builder()
+        .label(buttonLabel)
+        .margin_top(12)
+        .margin_bottom(12)
+        .margin_start(12)
+        .margin_end(12)
+        .build();
+
+    // Connect to "clicked" signal of `button`
+    button.connect_clicked(move |button| {
+        // Set the label to "Hello World!" after the button has been clicked on
+        match buttonLabel {
+            "Press me!" => (buttonLabel = "Hello World!"),
+            "Hello World!" => (buttonLabel = "Press me!"),
+        }
+
+        button.set_label(buttonLabel);
+    });
+
+    // Create a window
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("My GTK App")
+        .child(&button)
+        .build();
+
+    // Present window
+    window.present();
+}
