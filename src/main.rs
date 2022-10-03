53a7a58392d1f0e3b4c7b68b42fc3d9bfde8adce
amidg <amidg4x4dima@gmail.com>
2022-10-03 09:05:08 -0700

    added libadwaita

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,32 @@
+// main 'use' 
+use std::str;
+
+// gtk 'use'
 use gtk::prelude::*;
 use gtk::{Application, Button, ApplicationWindow};
 
-const APP_ID: &str = "org.gtk_rs.HelloWorld2";
+const APPLICATION_ID: &str = "org.gtk_rs.HelloWorld2";
+const APPLICATION_NAME: &str = "convertible - easy ffmpeg gui";
 
 fn main() {
     // Create a new application
-    let app = Application::builder().application_id(APP_ID).build();
-
+    let application = Application::new(Some(APPLICATION_ID), Default::default());
+    
     // Connect to "activate" signal of `app`
-    app.connect_activate(build_ui);
+    application.connect_activate(build_ui);
 
     // Run the application
-    app.run();
+    application.run();
+
+    
+//     let app = Application::builder().application_id(APP_ID).build();  
+//     app.connect_activate(build_ui);
+//     app.run();
 }
 
 fn build_ui(app: &Application) {
     // declare variable
-    let buttonLabel = "";
-
-    if buttonLabel == "" {
-        buttonLabel = "Press me!"
-    } else {
-        //
-    }
+    let mut buttonLabel = "";
 
     // Create a button with label and margins
     let button = Button::builder()
@@ -31,24 +35,26 @@ fn build_ui(app: &Application) {
         .margin_bottom(12)
         .margin_start(12)
         .margin_end(12)
-        .build();
+        .build();    
 
     // Connect to "clicked" signal of `button`
     button.connect_clicked(move |button| {
-        // Set the label to "Hello World!" after the button has been clicked on
-        match buttonLabel {
-            "Press me!" => (buttonLabel = "Hello World!"),
-            "Hello World!" => (buttonLabel = "Press me!"),
-        }
+        let newLabel = match buttonLabel {
+            "Press me!" => "Hello World!",
+            _=> "Press me!",
+        };
 
-        button.set_label(buttonLabel);
+        // Set the label to a new string after the button has been clicked on
+        button.set_label(newLabel);
     });
 
     // Create a window
     let window = ApplicationWindow::builder()
         .application(app)
-        .title("My GTK App")
+        .title(APPLICATION_NAME)
         .child(&button)
+        .default_height(800)
+        .default_width(800)
         .build();
 
     // Present window
